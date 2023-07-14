4c5af4e85a4bbf432f593fa76855e9e5ef3dfdbd
Avi Debnath <128149866+avideb24@users.noreply.github.com>
2023-07-14 16:43:37 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+
+
+    // $('.yt-opt').owlCarousel({
+    //     responsive: {
+    //         1200: {
+    //             items: 14
+    //         }
+    //     }
+    // });
+
+    
+    $('.search-icon').click(function(){
+        $('.search-bar input').toggleClass('search-input-show')
+    })
+
+
+    $('.sidebar-btn').click(function(){
+        $('.sidebar').toggleClass('sidebar-show')
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
